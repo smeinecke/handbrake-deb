ad7393cb8aeded7b85273971f8dabb33ad4846be
Stefan Meinecke <meinecke@greensec.de>
2022-04-09 01:54:04 +0200

    add docker files.

---
+++ scripts/build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+set -x
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "${SCRIPT}")
+WORKDIR="${PWD}"
+
+HB_TAG="1.5.1"
+
+git config --global advice.detachedHead false
+
+case "$(uname -i)" in
+  x86_64|amd64)
+    export SYSTEM_ARCH="x86_64"
+    SYSTEM_PLATFORM="x64";;
+  i?86)
+    export SYSTEM_ARCH="i686"
+    SYSTEM_PLATFORM="x86";;
+  *)
+    echo "Unsupported system architecture"
+    exit 1;;
+esac
+echo "System architecture: ${SYSTEM_PLATFORM}"
+
+case "${ARCH:-$(uname -i)}" in
+  x86_64|amd64)
+    TARGET_ARCH="x86_64"
+    PLATFORM="x64";;
+  i?86)
+    TARGET_ARCH="i686"
+    PLATFORM="x86";;
+  *)
+    echo "Unsupported target architecture"
+    exit 1;;
+esac
+echo "Target architecture: ${PLATFORM}"
+
+# Display tools version
+qmake --version
+cmake --version | head -n 1
+
+# Enable ccache
+export PATH="/usr/lib/ccache:${PATH}"
+export CCACHE_DIR="${WORKDIR}/cache/ccache"
+
+# Checkout handbreak
+cd "${WORKDIR}"
+if [[ -d HandBrake ]]; then
+  cd HandBrake
+  git clean -xdf
+  git fetch -t
+  git checkout master
+  git pull --ff-only
+  cd ..
+else
+  git clone --branch master https://github.com/HandBrake/HandBrake.git
+fi
+
+# If building from tag use a specific version of sources
+cd HandBrake
+if [[ -n "${HB_TAG}" ]]; then
+  echo "Checkout from tag: ${HB_TAG}"
+  git checkout "${HB_TAG}"
+fi
+COMMIT_HASH=$(git log -n 1 --pretty=format:'%h' --abbrev=8)
+
+mkdir -p build
+cd build
+../configure --enable-fdk-aac --enable-qsv --enable-vce --enable-nvenc --enable-x265 --enable-numa --enable-qsv --disable-gtk
