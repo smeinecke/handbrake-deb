70d6820990257b2b3f3b8d240cae0dc7e62aefe8
Stefan Meinecke <meinecke@greensec.de>
2022-04-09 15:59:24 +0200

    update deb filename.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,14 @@
 set -e
 set -x
 
+if [[ -z "${HB_TAG}" ]]; then
+    echo "Parameter HB_TAG missing!"
+fi
+
+if [[ -z "${DEB_FLAVOR}" ]]; then
+    echo "Parameter DEB_FLAVOR missing!"
+fi
+
 SCRIPT=$(readlink -f "$0")
 SCRIPTDIR=$(dirname "${SCRIPT}")
 WORKDIR="${PWD}"
@@ -30,14 +38,17 @@ fi
 
 # If building from tag use a specific version of sources
 cd HandBrake
-if [[ -n "${HB_TAG}" ]]; then
-  echo "Checkout from tag: ${HB_TAG}"
-  git checkout "${HB_TAG}"
-fi
+
+echo "Checkout from tag: ${HB_TAG}"
+git checkout "${HB_TAG}"
 COMMIT_HASH=$(git log -n 1 --pretty=format:'%h' --abbrev=8)
 
 # create original source tar file - just for dpkg-buildpackage compatibility
 git archive master | bzip2 > ../handbrake_${HB_TAG}.orig.tar.bz2
 cp -vr ${SCRIPTDIR}/debian-${DEB_FLAVOR} debian
-
 DEB_BUILD_OPTIONS="nocheck nodocs" dpkg-buildpackage -j$(nproc) -d -us -b
+cd ..
+rm -vf *-dbgsym.deb
+mv -v handbrake-cli_${HB_TAG}_amd64.deb handbrake-${DEB_FLAVOR}-cli_${HB_TAG}_amd64.deb
+mv -v handbrake_${HB_TAG}_amd64.deb handbrake-${DEB_FLAVOR}_${HB_TAG}_amd64.deb
+ls *.deb
