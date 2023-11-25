1b6f602cb9f58eccef6de696b415ac6f3cd0cde2
Stefan Meinecke <meinecke@greensec.de>
2023-11-25 18:30:20 +0100

    disable custom build for jammy

--- scripts/build.sh
+++ scripts/build.sh
@@ -50,31 +50,30 @@ fi
 cmake --version | head -n 1
 chmod  +x configure
 
-if [ "${DEB_FLAVOR}" == "jammy" ]; then
-  echo "* Workaround as build via normal dpkg-buildpackage does not work on jammy"
-  echo "* Build manually and use equivs to generate deb package"
-  ./configure \
-    --prefix=/usr \
-    --host=x86_64-linux-gnu \
-    --build=build/ \
-    --debug=std \
-    --enable-vce \
-    --enable-fdk-aac \
-    --enable-x265 \
-    --enable-qsv --enable-nvenc --enable-nvdec --enable-numa \
-    --launch-jobs=0 --launch
-  sed -i "s,_VERSION_,${HB_TAG}~${DEB_FLAVOR},g" ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian
-  sed -i "s,_VERSION_,${HB_TAG}~${DEB_FLAVOR},g" ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian-cli
-  # workaround to force Xz compression
-  echo -e 'override_dh_builddeb:' >>/usr/share/equivs/template/debian/rules
-  echo -e '\tdh_builddeb -- -Zxz' >>/usr/share/equivs/template/debian/rules
-  equivs-build -f --arch amd64 ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian
-  equivs-build -f --arch amd64 ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian-cli
-  mv -v *.deb ../
-  mv -v *.changes ../
-  mv -v *.buildinfo ../
-  exit 0;
-fi
+# if [ "${DEB_FLAVOR}" == "jammy" ]; then
+#   echo "* Workaround as build via normal dpkg-buildpackage does not work on jammy"
+#   echo "* Build manually and use equivs to generate deb package"
+#   ./configure \
+#     --prefix=/usr \
+#     --build=build/ \
+#     --debug=std \
+#     --enable-vce \
+#     --enable-fdk-aac \
+#     --enable-x265 \
+#     --enable-qsv --enable-nvenc --enable-nvdec --enable-numa \
+#     --launch-jobs=0 --launch
+#   sed -i "s,_VERSION_,${HB_TAG}~${DEB_FLAVOR},g" ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian
+#   sed -i "s,_VERSION_,${HB_TAG}~${DEB_FLAVOR},g" ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian-cli
+#   # workaround to force Xz compression
+#   echo -e 'override_dh_builddeb:' >>/usr/share/equivs/template/debian/rules
+#   echo -e '\tdh_builddeb -- -Zxz' >>/usr/share/equivs/template/debian/rules
+#   equivs-build -f --arch amd64 ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian
+#   equivs-build -f --arch amd64 ${SCRIPTDIR}/${DEB_FLAVOR}/equivs-debian-cli
+#   mv -v *.deb ../
+#   mv -v *.changes ../
+#   mv -v *.buildinfo ../
+#   exit 0;
+# fi
 
 # create original source tar file - just for dpkg-buildpackage compatibility
 git archive master | bzip2 > ../handbrake_${HB_TAG}.orig.tar.bz2
